cb15e1daea01a8fe00a945c618c278ca116e17f3
Admin <Ghimireadmen@gmail.com>
2026-05-07 09:55:44 +1200

    added the python code

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
-app = Flask(__name__)
-app.secret.key = 'TheSecretKey'
+app = Flask (__name__)
+app.secret_key = 'your_secret_key'
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)_
