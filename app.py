dc00a40a739fd2056e7bf6971d7d1ca24421d8d8
Admin <Ghimireadmen@gmail.com>
2026-05-07 09:37:02 +1200

    added code from slides and changed the html page title

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+app = Flask(__name__)
+app.secret.key = 'TheSecretKey'
+
+if __name__ == '__main__':
+    app.run(debug=True)
