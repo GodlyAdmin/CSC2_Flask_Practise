677b633cecef8d4915e0581473209d4a7d00fbdf
Admin <Ghimireadmen@gmail.com>
2026-05-11 09:20:01 +1200

    fixed the flask that was not working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask (__name__)
 app.secret_key = 'your_secret_key'
@@ -9,4 +9,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)_
+    app.run(debug=True)
+    
